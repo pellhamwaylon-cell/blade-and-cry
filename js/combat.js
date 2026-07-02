a173fd16145742898f95d242039f78e612f09589
pellhamwaylon-cell <pellhamwaylon@gmail.com>
2026-07-02 16:19:55 -0700

    Update combat.js

--- js/combat.js
+++ js/combat.js
@@ -1,23 +1,28 @@
 AFRAME.registerComponent('combat-weapon', {
   schema: {
-    type: { type: 'string', default: 'sword' }, 
-    swingRot: { type: 'vec3', default: {x: -60, y: 45, z: -45} } 
+    type: { type: 'string', default: 'sword' }
   },
   init: function () {
-    // When anywhere on the screen is clicked, swing!
     document.querySelector('a-scene').addEventListener('mousedown', () => {
       this.triggerAction();
     });
   },
   triggerAction: function() {
+    // 1. Visual Swing
     this.el.removeAttribute('animation__swing'); 
     this.el.setAttribute('animation__swing', {
       property: 'rotation',
-      to: `${this.data.swingRot.x} ${this.data.swingRot.y} ${this.data.swingRot.z}`,
+      to: `-60 45 -45`,
       dur: 150,          
       dir: 'alternate',  
       loop: 1,
       easing: 'easeInQuad'
     });
+
+    // 2. Throwing Logic (If it's a throwing knife)
+    if (this.data.type === 'throwable') {
+      console.log("Throwing mechanics triggered! (Requires detaching from grip and applying Ammo.js impulse)");
+      // Note: Real throwing requires detaching the entity from the camera rig and spawning it in world-space with velocity.
+    }
   }
 });
