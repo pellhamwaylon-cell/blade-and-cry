ed742bb089e5f3f9208e703232aaf10f97b285a3
pellhamwaylon-cell <pellhamwaylon@gmail.com>
2026-07-02 15:50:53 -0700

    Refactor weapon setup and hover text position
    Removed unnecessary comments and adjusted text position.

--- js/game.js
+++ js/game.js
@@ -36,16 +36,13 @@ window.spawnWeapon = function() {
   weapon.setAttribute('width', '0.1');
   weapon.setAttribute('position', '0 2 -2');
   weapon.setAttribute('ammo-body', 'type: dynamic; mass: 10');
+  weapon.setAttribute('combat-weapon', 'name: Golden Blade; type: sword; vrGrabMode: force;');
   
-  // Set up the modder components for the spawned weapon
-  weapon.setAttribute('combat-weapon', 'name: Golden Blade; type: sword; vrGrabMode: force; pcHoldPos: 0.3 -0.2 -0.6');
-  
-  // Create hover text
   const text = document.createElement('a-text');
   text.setAttribute('class', 'hover-text');
   text.setAttribute('value', 'Golden Blade');
   text.setAttribute('align', 'center');
-  text.setAttribute('position', '0 0.2 0');
+  text.setAttribute('position', '0 0.3 0');
   text.setAttribute('scale', '0.5 0.5 0.5');
   text.setAttribute('visible', 'false');
   text.setAttribute('color', 'gold');
